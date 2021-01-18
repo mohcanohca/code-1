9d31387bc6aedf60ccfc22db2307b877d1b36198
testname <testemail>
2021-01-18 13:50:27 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * @file app.js
+ * @author swan
+ */
+
+/* globals swan */
+
+App({
+    // new content 3
+    onLaunch(options) {
+        // do something when launch
+    },
+    onShow(options) {
+        // do something when show
+    },
+    onHide() {
+        // do something when hide
+    }
+});
